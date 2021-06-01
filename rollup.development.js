fb73bc2cfbfddddf033e56e137966b737f68d218
Martin Rossil <martinrossil@gmail.com>
2021-06-01 10:01:18 +0200

    Created dev and build files.

---
+++ rollup.development.js
@@ -0,0 +1,25 @@
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+
+export default [{
+        input: './src/AltOmFilmAdmin.ts',
+        plugins: [
+            typescript({ tsconfig: 'tsconfig.development.json' }),
+            resolve(),
+            serve({
+                contentBase: 'development',
+                open: true
+            }),
+            livereload({
+                watch: 'development'
+            })
+        ],
+        output: {
+            file: './development/AltOmFilmAdmin.js',
+            format: 'esm',
+            sourcemap: 'inline'
+        }
+    }
+]
